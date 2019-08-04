65696b5d32ca8e7f4a52c45e616d6e3472e0bf8f
akourtis98 <akourtisdev@gmail.com>
2019-08-04 21:34:19 +0300

    submit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact.model';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,138 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'work2';
+  first: string = "";
+  last: string = "";
+  first_name: string = "";
+  last_name: string = "";
+  // email: string = "";
+  // office_phone: string = "";
+  // street: string = "";
+  // zip_code: string = "";
+
+  contacts = [];
+  enabled = false;
+  enabledEdit = false;
+  view = false;
+  enableViewId;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+
+  addContact(id) {
+    this.contacts.push({
+      first_name: this.first_name,
+      last_name: this.last_name,
+      id: ++this.contacts[this.contacts.length - 1].id
+      // email: this.email,
+      // office_phone: this.office_phone,
+      // street: this.street,
+      // zip_code: this.zip_code
+    });
+
+    // dataService.addContact({
+    // fields
+    //})
+
+    // refresh
+
+    this.cancel();
+    // show success msg
+  }
+
+  cancel() {
+    this.first_name = "";
+    this.last_name = "";
+
+    this.enabled = false;
+  }
+
+  cancelSubmitted(id) {
+
+    let y = this.contacts.findIndex(x => x.id === id);
+
+    this.contacts[y] = ({
+      first_name: this.contacts[y].first_name,
+      last_name: this.contacts[y].last_name,
+      id: this.contacts[y].id
+    });
+
+    this.enableViewId = null;
+    this.enabledEdit = false;
+  }
+
+  deleteContact(id: number) {
+    console.log("id: " + id);
+    const pos = id;
+    this.contacts.splice(pos, 1);
+
+    // dataService.deleteContact
+  }
+
+  enableView(id) {
+    this.view = true;
+
+    console.log("enableView: " + id);
+
+    this.enableViewId = id;
+
+    // let y = this.contacts.findIndex(x => x.id === id);
+
+
+    // this.contacts[y].class
+
+    // this.contacts.push({
+    //   first_name:  this.contacts[y].first_name,
+    //   last_name:  this.contacts[y].last_name,
+    //   // email: this.email,
+    //   // office_phone: this.office_phone,
+    //   // street: this.street,
+    //   // zip_code: this.zip_code
+    // });
+  }
+
+  enableEdit() {
+    this.enabledEdit = true;
+  }
+
+  enable(id: number) {
+    this.enabled = true;
+  }
+  private regForm: any;
+
+  edit(id) {
+    let y = this.contacts.findIndex(x => x.id === id);
+    // console.log(this.regForm.get('first').value);
+
+    this.contacts[y] = ({
+      first_name: (<HTMLInputElement>document.getElementById("first")).value,
+      last_name: (<HTMLInputElement>document.getElementById("last")).value,
+      id: id
+    });
+
+    this.enableViewId = null;
+    this.enabledEdit = false;
+  }
+
+  ngOnInit() {
+    this.contacts.push({
+      first_name: "first_name",
+      last_name: "this.last_name",
+      id: 0
+      // email: this.email,
+      // office_phone: this.office_phone,
+      // street: this.street,
+      // zip_code: this.zip_code
+    }, {
+        first_name: "john",
+        last_name: "alex",
+        id: 1
+      });
+
+    this.regForm = this.formBuilder.group({
+      first: [''],
+      last: ['']
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
